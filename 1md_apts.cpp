bd154afa4bb638b208d3cb4dff191d23b49d7807
Johans Justs Eris <johans.eris@r6vsk.lv>
2023-03-11 17:55:12 +0200

    Add 1md_apts.cpp, add plan to README.md

---
+++ 1md_apts.cpp
@@ -0,0 +1,22 @@
+/**
+*Nr 1-9
+Laiks 2 000 000 000
+Atpūšas, kad simtnieku pozīcijas cipas sakrīt ar nr: 5 un 500-590, 1500-1599 utt
+
+
+out - k beigu moments, f nr., k nr.
+secīgi laikam pieaugot. Ja vienā laika momentā beidz apkalpot vairākus klientus, tad rezultāti jādrukā frizieru numuru secībā.
+ **/
+
+#include <iostream>
+#include <fstream>
+using namespace std
+
+int main() {
+
+}
+
+ /**
+ * nigga
+ */
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # apts-1md-mati
 University homework.
+
+TODO:
+Pland:
+- Open and read file
+- Make a linked list of the barbers
+- Make a linked list of the customers
+- Global variable for the time.
+- Run the program (misc functions).
