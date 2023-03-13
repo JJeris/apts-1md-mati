0024b94269bf5ee5e50a7b075bc8a26be4a22f4b
Johans Justs Eris <johans.eris@r6vsk.lv>
2023-03-13 21:32:55 +0200

    Add basic linked-list functionality

--- 1md_apts.cpp
+++ 1md_apts.cpp
@@ -12,64 +12,138 @@ secīgi laikam pieaugot. Ja vienā laika momentā beidz apkalpot vairākus klien
 #include <fstream>
 using namespace std;
 
-int main()
-{
-    // Open the file
-    ifstream file("hair.in");
-    // Check if the file was opened successfully
-    if (!file.is_open())
-    {
-        cout << "Failed to open file." << endl;
-        return 1;
+
+// Definition of a Node struct with an integer value and a pointer to the next Node.
+// Used to construct a linked list.
+struct Node {
+    int value;
+    Node* next;
+};
+
+// Traverse the linked list and print out each value
+void traverseList(Node* head) {
+    Node* current = head;
+    while (current != nullptr) {
+        std::cout << current->value << " ";
+        current = current->next;
     }
-    
+}
 
-    // Read each line of the file
-    // string line;
-    // for (int i = 1; getline(file, line); ++i)
-    // {
-    //     // Output the line to the console
-    //     cout << "Line " << i << ": " << line << endl;
-    // }
-    // int foo;
-    // getline(file, foo);
-
-    // Read the first line of the file into an integer variable named "foo"
-    int foo;
-    file >> foo;
-
-    // Output the first line to the console
-    cout << "The first line of the file is: " << foo << endl;
-
-    // Read and output each integer of the file
-    int num;
-    while (file >> num)
-    {
-        cout << "Number: " << num << endl;
+// Delete the nodes in the linked list
+void deleteList(Node** headRef) {
+    Node* current = *headRef;
+    while (current != nullptr) {
+        Node* temp = current;
+        current = current->next;
+        delete temp;
     }
+    *headRef = nullptr;
+}
 
-    
-    // string word;
-    // while (file >> word)
-    // {
-    //     // Output the word to the console
-    //     cout << "Word: " << word << endl;
+// Creates a linked-list
+void createList(Node** headRef, int n) {
+    *headRef = nullptr;
+
+    // Create the linked list
+    for (int i = 1; i <= n; i++) {
+        Node* newNode = new Node;
+        newNode->value = i;
+        newNode->next = nullptr;
+
+        if (*headRef == nullptr) {
+            *headRef = newNode;
+        }
+        else {
+            Node* current = *headRef;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+    // // Create the first node
+    // *headRef = new Node;
+    // (*headRef)->value = 1;
+    // (*headRef)->next = nullptr;
+
+    // // Create the remaining nodes
+    // Node* current = *headRef;
+    // for (int i = 2; i <= n; i++) {
+    //     Node* newNode = new Node;
+    //     newNode->value = i;
+    //     newNode->next = nullptr;
+    //     current->next = newNode;
+    //     current = newNode;
     // }
+}
 
-
-    // Read the first line into a char array
-    // const int MAX_LINE_LENGTH = 256; // Maximum line length to read
-    // char line[MAX_LINE_LENGTH];
-    // file.getline(line, MAX_LINE_LENGTH);
-    
-    // Output the first line to the console
-    // cout << "The first line of the file is: " << line << endl;
-    
-    // Close the file
-    file.close();
-    
+int main() {
+    int n = 6;
+    Node* head = nullptr;
+    createList(&head, n);
+    traverseList(head);
+    deleteList(&head);
     return 0;
 }
+
+// int main()
+// {
+//     // Open the file
+//     ifstream file("hair.in");
+//     // Check if the file was opened successfully
+//     if (!file.is_open())
+//     {
+//         cout << "Failed to open file." << endl;
+//         return 1;
+//     }
+    
+
+//     // Read each line of the file
+//     // string line;
+//     // for (int i = 1; getline(file, line); ++i)
+//     // {
+//     //     // Output the line to the console
+//     //     cout << "Line " << i << ": " << line << endl;
+//     // }
+//     // int foo;
+//     // getline(file, foo);
+
+//     // Read the first line of the file into an integer variable named "foo"
+//     int foo;
+//     file >> foo;
+
+//     // Output the first line to the console
+//     cout << "The first line of the file is: " << foo << endl;
+
+//     // Read and output each integer of the file
+//     int num;
+//     while (file >> num)
+//     {
+//         cout << "Number: " << num << endl;
+//     }
+
+    
+//     // string word;
+//     // while (file >> word)
+//     // {
+//     //     // Output the word to the console
+//     //     cout << "Word: " << word << endl;
+//     // }
+
+
+//     // Read the first line into a char array
+//     // const int MAX_LINE_LENGTH = 256; // Maximum line length to read
+//     // char line[MAX_LINE_LENGTH];
+//     // file.getline(line, MAX_LINE_LENGTH);
+    
+//     // Output the first line to the console
+//     // cout << "The first line of the file is: " << line << endl;
+    
+//     // Close the file
+//     file.close();
+    
+//     return 0;
+// }
 
 
 
