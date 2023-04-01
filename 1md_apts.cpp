f546ed6d5c3aec2394d3456226902d84a1646edd
Johans Justs Eris <johans.eris@r6vsk.lv>
2023-04-01 20:47:57 +0300

    Commit

--- 1md_apts.cpp
+++ 1md_apts.cpp
@@ -1,7 +1,9 @@
 #include <fstream>
 using namespace std;
+// Author: Johans Justs Eris, je21033
 
-// Definition of a Barber struct with an integer value and a pointer to the next Node. Used to construct a linked list.
+// Definition of a Barber struct with an integer value 
+// and a pointer to the next Node. Used to construct the barber linked list.
 struct Barber {
     int numB;
     int lastWorkTime = 0;
@@ -10,7 +12,7 @@ struct Barber {
     Barber* next;
 };
 
-// Delete the nodes in the linked list
+// Delete the nodes in the barber linked list.
 void deleteList(Barber** headRef) {
     Barber* current = *headRef;
     while (current != nullptr) {
@@ -21,61 +23,75 @@ void deleteList(Barber** headRef) {
     *headRef = nullptr;
 }
 
-// Creates a linked-list
+// Create the barber linked list.
 void createList(Barber** headRef, int n) {
     *headRef = nullptr;
 
-    // Create the linked list
+    // Create the linked list.
     for (int i = 1; i <= n; i++) {
-        Barber* newNode = new Barber;
-        newNode->numB = i;
-        newNode->next = nullptr;
+        Barber* newBarber = new Barber;
+        newBarber->numB = i;
+        newBarber->next = nullptr;
 
         if (*headRef == nullptr) {
-            *headRef = newNode;
+            *headRef = newBarber;
         }
         else {
             Barber* current = *headRef;
             while (current->next != nullptr) {
                 current = current->next;
             }
-            current->next = newNode;
+            current->next = newBarber;
         }
     }
 }
 
-// This function sorts a linked list of Barber nodes in ascending order based on their lastWorkTime and numB attributes. It uses a selection sort algorithm to find the minimum element from the current node to the end of the list, and swaps it with the current node if necessary. The function takes a pointer to the head of the linked list as input and modifies the list in place.
+// This function sorts a linked list of Barber nodes in ascending order 
+// based on their lastWorkTime and numB values. It uses a selection 
+// sort algorithm to find the minimum element from the current node to the 
+// end of the list, and swaps it with the current node if necessary.
 void sortList(Barber** headRef) {
     Barber* current = *headRef;
     Barber* min;
 
     while (current != nullptr) {
-        // Find the minimum element from current to the end of the list
+        // Find the minimum element from current to the end of the list.
         min = current;
         Barber* innerCurrent = current->next;
         while (innerCurrent != nullptr) {
-
             if (!innerCurrent->busy && min->busy ||
                 (innerCurrent->busy == min->busy && innerCurrent->lastWorkTime < min->lastWorkTime) ||
                 (innerCurrent->busy == min->busy && innerCurrent->lastWorkTime == min->lastWorkTime && innerCurrent->numB < min->numB)) {
                 min = innerCurrent;
             }
-
-
             innerCurrent = innerCurrent->next;
         }
-        // Swap the minimum element with the current element
+        // Swap the minimum element with the current element.
         if (min != current) {
-            swap(current->numB, min->numB);
-            swap(current->lastWorkTime, min->lastWorkTime);
-            swap(current->busy, min->busy);
-            swap(current->client, min->client);
+            int temp = current->numB;
+            current->numB = min->numB;
+            min->numB = temp;
+
+            temp = current->lastWorkTime;
+            current->lastWorkTime = min->lastWorkTime;
+            min->lastWorkTime = temp;
+
+            bool tempBool = current->busy;
+            current->busy = min->busy;
+            min->busy = tempBool;
+
+            temp = current->client;
+            current->client = min->client;
+            min->client = temp;
         }
         current = current->next;
     }
 }
-// Nosaka, vai barberis ir derīgs, skatoties uz dažādiem vairantiem, kad klients varētu atnākt un vai viņa pauze viņam kko sapisīs. Skaties image.png.
-bool valid(int barberNum, int time, int duration){
+
+// Finds out if a barber is allowed (is valid) to service a client, 
+// considering the time, when the client arrives, and if the barbers break 
+// time would interfere with the appointment.
+bool isValidBarber(int barberNum, int time, int duration){
     int hundrethPlace = (time / 100) % 10;
     int hundrethPart = time % 1000;
     if (hundrethPlace == barberNum){
@@ -94,37 +110,41 @@ bool valid(int barberNum, int time, int duration){
 }
 
 int main() {
-    // Gloabl variabes
+    // Global time.
     long int currentTime = 0;    
 
-    // Open the files
+    // Open the files.
     ifstream input("hair.in");
     ofstream output;
 
-
-    // Initialization
+    // Create a buffer, to help with the output. Makes the program faster 
+    // and in turn more effiecient.
     const unsigned int length = 1536;
     char buffer[length];
     output.rdbuf()->pubsetbuf(buffer, length);
     output.open("hair.out");
 
-    // Create a linked list of barbers
+    // Initialize variables.
     int n = 0;
     int time = 0; 
     int clientNum = 0; 
     int duration = 0;
     int cursor = 0;
     char line[25];
+
+    // Read in the number of barbers.
     input.getline(line, 25);
     while(line[cursor] != '\0'){
         n = n*10 + line[cursor] - '0';
         cursor++;
     }
     cursor = 0;
+
+    // Create a linked list of barbers.
     Barber* head = nullptr;
     createList(&head, n);
 
-    
+    // Read in the time, client number and duration of the appointment for the first client.
     input.getline(line, 25);
     while(line[cursor] != ' '){
         time = time*10 + line[cursor] - '0';
@@ -144,130 +164,177 @@ int main() {
 
     Barber* current = head;
 
-    //! ŠIS VISS IR PIRMAJAM ELEMENTAM Atrod derīgo barberi.
+    // First loop for that processes the first client.
     while(true) {
         if(current == nullptr){
-            // exit(1);
-            time = (((time/100)*100)+100);
+            // If the list has ended, update the time, to mitigate the 
+            // barbers break time.
+            time = (((time/100)*100)+100); // Update the time by 100.
             currentTime = time;
             current = head;
         }
-        if (valid(current->numB, time, duration)){
-            // klientu, laiku, busy.
+        if (isValidBarber(current->numB, time, duration)){
+            // If the barber can service the client - add the values to 
+            // the barbers node.
             current->lastWorkTime = time+duration-1;
             current->client = clientNum;
             current->busy = true;
-            sortList(&head); // aiznemts, vai nē, pēc laika, un tad nr.
+            sortList(&head); // The list gets sorted immediately for convenience.
             break;
         } 
         else {
-            // nederīgs
+            // If the current barber wasn't valid, go to the next one.
             current = current->next;
             continue;
         }        
     }
 
-    // <...>
-    // if (!='0'){
-    //     while
-    // }
-    // else {
+    time = 0; 
+    clientNum = 0; 
+    duration = 0;
+    cursor = 0;
 
-    // }
-    // ! ŠIS VISS IR NĀKAMAJIEM ELEMENTIEM
-    while (input >> time >> clientNum >> duration) { //true
-    // while(time != 0)
-        if (time == 0) break; // Tātad fails beidzās
-        current = head;
-        if (time < currentTime){ // Ātrākais brīdis, kad klientu barberis var apkalpot ķip
-            time = currentTime;
-            currentTime = time;
+    // Read the next client.
+    input.getline(line, 25);
+    if (line[cursor]!='0'){ // Checks, weather the file has ended.
+        while(line[cursor] != ' '){
+            time = time*10 + line[cursor] - '0';
+            cursor++;
         }
-        // Atbrīvot busy: salīdzini time ar vinu lastWorkTime
-        while(current != nullptr && current->lastWorkTime < time){
-            if (current->busy == true) {
-                current->busy = false;
-                output << current->lastWorkTime << " " << current->numB << " " << current->client << "\n";
+        cursor++;
+        while(line[cursor] != ' '){
+            clientNum = clientNum*10 + line[cursor] - '0';
+            cursor++;
+        }
+        cursor++;
+        while(line[cursor] != '\0'){
+            duration = duration*10 + line[cursor] - '0';
+            cursor++;
+        }
+        cursor = 0;
+
+        // Continue to process the remaining clients inside hair.in.
+        while (true) {
+            current = head;
+            if (time < currentTime){ // The next soonest moment, that a client can be serviced.
+                time = currentTime;
+                currentTime = time;
             }
-            current = current->next;
-        }
-        current = head;
-        bool busyExists = false;
-        while(true) {
-           
-            if(current == nullptr){
-                // tikai brivie frizieri
-                // tikai aiznemtie frizieri
-                // abi kopā sarakstā.
-                if (busyExists == false){
-                    // All not busy
-                    time = (((time/100)*100)+100);
-                    currentTime = time;
-                    current = head;
+            // Free up the busy barbers and output their content.
+            while(current != nullptr && current->lastWorkTime < time){
+                if (current->busy == true) {
+                    current->busy = false;
+                    output << current->lastWorkTime << " " << current->numB << " " << current->client << "\n";
                 }
-                else if (busyExists == true){
-                    current = head;
-                    if (current->busy == true){
-                        // All busy
-                        currentTime = current->lastWorkTime+1;
-                        time = currentTime;
+                current = current->next;
+            }
+            current = head;
+            // All barbers are no longer busy.
+            bool busyExists = false;
+            while(true) {
+                // Updates the currentTime, considering if all barbers are free, 
+                // all barbers are busy or its a mix of both.
+                if(current == nullptr){
+                    if (busyExists == false){
+                        // All barbers are free.
+                        time = (((time/100)*100)+100);
+                        currentTime = time;
+                        current = head;
                     }
-                    else {
-                        while (current->busy != true) {
-                            current = current->next;
-                        }
-                        // Both busy and not busy
-                        if (time/100*100+100<current->lastWorkTime+1) {
-                            currentTime = time/100*100+100;
-                            time = currentTime;    
-                        }
-                        else {
+                    else if (busyExists == true){
+                        current = head;
+                        if (current->busy == true){
+                            // All barbers are busy
                             currentTime = current->lastWorkTime+1;
                             time = currentTime;
                         }
-                    }
-                }
-
-                current = head;
-                while(current != nullptr && current->lastWorkTime < time){
-                    // Ja ir currentam ir beidzies tas iepriekšējais klienta apkalpošana
-                    if (current->busy == true) {
-                        // Ja next ir nullptr, tad lists ie beidzies.
-                        if (current->next == nullptr) {
-                            // Un tas nozīmē, ka neeksistē vairs neviens busy barber, so nomainam busyExists uz false, jo no busy exist.
-                            busyExists = false;
+                        else {
+                            while (current->busy != true) {
+                                current = current->next;
+                            }
+                            // The barber list has Both busy and free barbers.
+                            if (time/100*100+100<current->lastWorkTime+1) {
+                                currentTime = time/100*100+100;
+                                time = currentTime;    
+                            }
+                            else {
+                                currentTime = current->lastWorkTime+1;
+                                time = currentTime;
+                            }
                         }
-                        // Ja tas nav vēl beidzies, tad vnk nomainam viņa busy uz false uz sūtam pabeigtā klienta datus prom.
-                        current->busy = false;
-                        output << current->lastWorkTime << " " << current->numB << " " << current->client << "\n";
                     }
-                    current = current->next;
+
+                    current = head;
+                    while(current != nullptr && current->lastWorkTime < time){
+                        // While the current barber has finished servicing his client.
+                        if (current->busy == true) {
+                            // If the next barber is empty - the list has ended.
+                            if (current->next == nullptr) {
+                                // Which means that there are no more busy barbers in the list.
+                                busyExists = false;
+                            }
+                            // If the list hasn't ended yet, then only set the current->busy to false, 
+                            // and output the contents of the element.
+                            current->busy = false;
+                            output << current->lastWorkTime << " " << current->numB << " " << current->client << "\n";
+                        }
+                        current = current->next;
+                    }
+                    current = head;
                 }
-                current = head;
+                if (current->busy == true){ 
+                    // When the list no longer has any free barbers, set busyExist to true, 
+                    // and skip to the end of the list.
+                    busyExists = true;
+                    current = nullptr;
+                    continue;
+                }
+                if (current->busy == false && isValidBarber(current->numB, time, duration)){
+                    // When the barber can service the client, pass him the clients information.
+                    current->lastWorkTime = time+duration-1;
+                    current->client = clientNum;
+                    current->busy = true;
+                    sortList(&head); // Again sort the list.
+                    break;
+                } 
+                else {
+                    // If the barber is not valid.
+                    current = current->next;
+                    continue;
+                }        
+            }    
+        
+        time = 0; 
+        clientNum = 0; 
+        duration = 0;
+        cursor = 0;
+        // Read in the next client.
+        input.getline(line, 25);
+        if (line[cursor]!='0'){
+            while(line[cursor] != ' '){
+                time = time*10 + line[cursor] - '0';
+                cursor++;
             }
-            if (current->busy == true){ // Kad sarakstā vairs nav neviena brīva friziera, un sastapam pirmo busy frizieru, tad nav jēgas vairs checkot
-                busyExists = true;
-                current = nullptr;
-                continue;
+            cursor++;
+            while(line[cursor] != ' '){
+                clientNum = clientNum*10 + line[cursor] - '0';
+                cursor++;
             }
-            if (current->busy == false && valid(current->numB, time, duration)){
-                // klientu, laiku, busy.
-                current->lastWorkTime = time+duration-1;
-                current->client = clientNum;
-                current->busy = true;
-                sortList(&head); // aiznemts, vai nē, pēc laika, un tad nr.
-                break;
-            } 
-            else {
-                // nederīgs
-                current = current->next;
-                continue;
-            }        
-        }    
-    // <...>
+            cursor++;
+            while(line[cursor] != '\0'){
+                duration = duration*10 + line[cursor] - '0';
+                cursor++;
+            }
+            cursor = 0;
+        }
+        else {
+            break;
+        }
+
+        }
     }
     current = head;
-    // Kas viss beidzies, vnk iztukšo arī tos, kas vēl bija busy.
+    // When the input file has ended, output all of the busy barbers, if there were any.
     while(current != nullptr){
             if (current->busy == true) {
                 output << current->lastWorkTime << " " << current->numB << " " << current->client << "\n";
@@ -275,11 +342,11 @@ int main() {
             current = current->next;
         }
 
-    // Close the file
+    // Close the files.
     input.close();
     output.close();
 
-    // Free memory
+    // Free up memory by deleting the list.
     deleteList(&head);
     return 0;
 }
---
+++ aptsMati.cpp
@@ -0,0 +1,350 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Frizieris
+{
+    int num = 0;
+    int pedejaisDarbaLaiks = 0;
+    int klients = 0;
+    Frizieris* next;
+};
+
+void izveidotSarakstu(Frizieris** head, int n)
+{
+    *head = nullptr;
+
+    // izveido sarakstu
+    for (int i = 1; i <= n; i++)
+    {
+        Frizieris* frizieris = new Frizieris;
+        frizieris->num = i;
+        frizieris->next = nullptr;
+
+        //ja saraksts ir tukšs
+        if (*head == nullptr)
+        {
+            *head = frizieris;
+        }
+
+        //ja saraksts nav tukšs
+        else
+        {
+            //iet cauri sarakstam
+            Frizieris* current = *head;
+            while (current->next != nullptr)
+            {
+                current = current->next;
+            }
+            //un beigās pieliek jauno frizieri
+            current->next = frizieris;
+        }
+    }
+}
+
+void izdzestSarakstu(Frizieris** head)
+{
+    Frizieris* current = *head;
+    while (current != nullptr)
+    {
+        Frizieris* temp = current;
+        current = current->next;
+        delete temp;
+    }
+    *head = nullptr;
+}
+
+void pievienotElementu(Frizieris** head, int num, int pedejaisDarbaLaiks, int klients)
+{
+    Frizieris* frizieris = new Frizieris;
+    frizieris->num = num;
+    frizieris->pedejaisDarbaLaiks = pedejaisDarbaLaiks;
+    frizieris->klients = klients;
+    frizieris->next = nullptr;
+
+    //ja saraksts ir tukšs
+    if (*head == nullptr)
+    {
+        *head = frizieris;
+    }
+
+    //ja saraksts nav tukšs
+    else
+    {
+        //iet cauri sarakstam
+        Frizieris* current = *head;
+        while (current->next != nullptr)
+        {
+            current = current->next;
+        }
+
+        //un beigās pieliek jauno frizieri
+        current->next = frizieris;
+    }
+}
+
+void izdzestElementu(Frizieris** head)
+{
+    //ja ir tukšs saraksts, tad nav, ko dzēst
+    if (*head == nullptr)
+    {
+        return;
+    }
+
+    //izdzēš pirmo elementu un norāda saraksta 2.elementu kā jauno 1.elementu
+    Frizieris* temp = *head;
+    *head = (*head)->next;
+    delete temp;
+}
+
+void kartosana(Frizieris** head)
+{
+    // saraksts ir tukšs vai sarakstā ir tikai viens elements
+    if (*head == nullptr || (*head)->next == nullptr)
+    {
+        return;
+    }
+
+    Frizieris* previous = nullptr;
+    Frizieris* current = *head;
+    Frizieris* next = nullptr;
+    bool swapped;
+
+    //dara kamēr elementi ir jāmaina vietām
+    do
+    {
+        swapped = false;
+        current = *head;
+
+        //kamēr nav sasniegtas saraksta beigas
+        while (current->next != nullptr)
+        {
+            next = current->next;
+
+            //ja ir atrasti divi elementi, kuri ir jāmaina vietām
+            if (current->pedejaisDarbaLaiks > next->pedejaisDarbaLaiks ||
+                    (current->pedejaisDarbaLaiks == next->pedejaisDarbaLaiks && current->num > next->num))
+            {
+                swapped = true;
+
+                //ja nav pirmais elements
+                if (previous != nullptr)
+                {
+                    previous->next = next;
+                }
+                //ja ir pirmais elements
+                else
+                {
+                    *head = next;
+                }
+
+                //apmaina elementus vietām
+                current->next = next->next;
+                next->next = current;
+
+                previous = next;
+                next = current->next;
+            }
+
+            //apskata nākamo elementu
+            else
+            {
+                previous = current;
+                current = next;
+                next = next->next;
+            }
+        }
+    }
+    while (swapped);
+}
+
+bool derigs(int frizieraNum, int laiks, int pateretaisLaiks)
+{
+    int simti = laiks % 1000;
+
+    if ((simti/100) == frizieraNum)
+    {
+        return false;
+    }
+
+    else
+    {
+        int beiguLaiks = simti + pateretaisLaiks - 1;
+        int partraukums = frizieraNum*100;
+
+        if(simti < partraukums && beiguLaiks >= partraukums)
+        {
+            return false;
+        }
+
+        else if(simti > (partraukums+99) && beiguLaiks >= (partraukums+1000))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+int main()
+{
+    //laiks vajadzīgs, lai pārbaudītu brīvos frizierus
+    int reallaiks = 0;
+
+    //atver failus
+    ifstream input("hair.i3");
+    ofstream output("hair.out");
+
+    int n;
+    input >> n;
+    Frizieris* headBrivie = nullptr;
+    izveidotSarakstu(&headBrivie, n);
+
+    Frizieris* headAiznemtie = nullptr;
+
+    int laiks;
+    int klientaNum;
+    int pateretaisLaiks;
+
+    input >> laiks >> klientaNum >> pateretaisLaiks;
+
+    Frizieris* currentBrivie = headBrivie;
+    Frizieris* currentAiznemtie = headAiznemtie;
+
+    while(true)
+    {
+        //ja nav neviena brīva friziera
+        if(currentBrivie == nullptr)
+        {
+            laiks = (((laiks/100)*100)+100);
+            reallaiks = laiks;
+            currentBrivie = headBrivie;
+        }
+
+        if(derigs(currentBrivie->num, laiks, pateretaisLaiks))
+        {
+            int darbaLaiks = laiks + pateretaisLaiks - 1;
+            pievienotElementu(&headAiznemtie, currentBrivie->num, darbaLaiks, klientaNum);
+            izdzestElementu(&headBrivie);
+            break;
+        }
+
+        else //dotais frizieris ies pārtraukumā, tātad neder
+        {
+            currentBrivie = currentBrivie->next;
+            continue;
+        }
+    }
+
+    while (input >> laiks >> klientaNum >> pateretaisLaiks)
+    {
+        if (laiks == 0) break; //faila beigas
+
+        currentBrivie = headBrivie;
+        currentAiznemtie = headAiznemtie;
+
+        if(laiks < reallaiks+1)
+        {
+            laiks = reallaiks+1;
+            reallaiks = laiks;
+        }
+        currentAiznemtie = headAiznemtie;
+        while(currentAiznemtie != nullptr && currentAiznemtie->pedejaisDarbaLaiks < laiks)
+        {
+            output << currentAiznemtie->pedejaisDarbaLaiks << " " << currentAiznemtie->num << " " << currentAiznemtie->klients << '\n';
+            pievienotElementu(&headBrivie, currentAiznemtie->num, currentAiznemtie->pedejaisDarbaLaiks, currentAiznemtie->klients);
+            izdzestElementu(&headAiznemtie);
+            kartosana(&headBrivie);
+            currentAiznemtie = currentAiznemtie->next;
+        }
+
+        currentAiznemtie = headAiznemtie;
+
+        while(true)
+        {
+            if (currentBrivie == nullptr)
+            {
+                if(headAiznemtie == nullptr)
+                {
+                    laiks = (((laiks/100)*100)+100);
+                    reallaiks = laiks;
+                    currentBrivie = headBrivie;
+                }
+                else
+                {
+                    currentBrivie = headBrivie;
+                    currentAiznemtie = headAiznemtie;
+                    if (currentBrivie == nullptr)
+                    {
+                        reallaiks = currentAiznemtie->pedejaisDarbaLaiks+1;
+                        laiks = reallaiks;
+                    }
+                    else
+                    {
+                        if((((laiks/100)*100)+100) < currentAiznemtie->pedejaisDarbaLaiks+1)
+                        {
+                            reallaiks = (((laiks/100)*100)+100);
+                            laiks = reallaiks;
+                        }
+                        else
+                        {
+                            //...
+                            kartosana(&headAiznemtie);
+                            reallaiks = headAiznemtie->pedejaisDarbaLaiks+1;
+                            laiks = reallaiks;
+                        }
+                    }
+                }
+
+                currentAiznemtie = headAiznemtie;
+
+                while(currentAiznemtie != nullptr && currentAiznemtie->pedejaisDarbaLaiks < laiks)
+                {
+                    output << currentAiznemtie->pedejaisDarbaLaiks << " " << currentAiznemtie->num << " " << currentAiznemtie->klients << '\n';
+                    pievienotElementu(&headBrivie, currentAiznemtie->num, currentAiznemtie->pedejaisDarbaLaiks, currentAiznemtie->klients);
+                    izdzestElementu(&headAiznemtie);
+                    kartosana(&headBrivie);
+                    currentAiznemtie = currentAiznemtie->next;
+                }
+
+                currentBrivie = headBrivie;
+            }
+
+            if(currentBrivie == nullptr)
+            {
+                continue;
+            }
+
+            if(derigs(currentBrivie->num, laiks, pateretaisLaiks))
+            {
+                int darbaLaiks = laiks + pateretaisLaiks - 1;
+                pievienotElementu(&headAiznemtie, currentBrivie->num, darbaLaiks, klientaNum);
+                izdzestElementu(&headBrivie);
+                kartosana(&headAiznemtie);
+                break;
+            }
+
+            else
+            {
+                currentBrivie = currentBrivie->next;
+                continue;
+            }
+        }
+    }
+
+    currentAiznemtie = headAiznemtie;
+
+    while(currentAiznemtie != nullptr)
+    {
+        output << currentAiznemtie->pedejaisDarbaLaiks << " " << currentAiznemtie->num << " " << currentAiznemtie->klients << '\n';
+        currentAiznemtie = currentAiznemtie->next;
+    }
+
+    input.close();
+    output.close();
+
+    izdzestSarakstu(&headBrivie);
+    izdzestSarakstu(&headAiznemtie);
+
+    return 0;
+}
